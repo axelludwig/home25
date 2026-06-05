d332097a5069d0d813b498876364db83744acff0
axel ludwig <axel.ludwig@outlook.com>
2026-06-06 00:30:56 +0200

    feat(api): update backend entrypoint

--- home25back/index.js
+++ home25back/index.js
@@ -8,35 +8,35 @@ const PORT = process.env.PORT || 3000;
 app.use(express.json());
 app.use(cors());
 
-// Route de test
+// Test route
 app.get('/', (req, res) => {
-    res.send(`Hello, World! 🌍 (clé API : ${process.env.API_KEY || 'Non définie'})`);
+    res.send(`hello, world 🌍 (api key: ${process.env.API_KEY || 'not defined'})`);
 });
 
 app.get('/weather', (req, res) => {
     const { lat, lon } = req.query;
 
     if (!lat || !lon) {
-        return res.status(400).json({ error: 'Veuillez fournir lat et lon' });
+        return res.status(400).json({ error: 'please provide lat and lon' });
     }
 
     fetch(`https://api.openweathermap.org/data/2.5/weather?lat=${lat}&lon=${lon}&appid=${process.env.API_KEY}&units=metric`)
         .then(response => response.json())
         .then(data => {
             res.json(data);
-            // res.json({ message: `Données météo pour lat: ${lat}, lon: ${lon} 🌤️` });
+            // res.json({ message: `Weather data for lat: ${lat}, lon: ${lon} 🌤️` });
         })
         .catch(error => {
-            res.status(500).json({ error: 'Erreur lors de la récupération des données météo' });
+            res.status(500).json({ error: 'error retrieving weather data' });
         });
 });
 
 
 app.get('/api/data', (req, res) => {
-    res.json({ message: 'Voici des données JSON 🚀' });
+    res.json({ message: 'here\'s json data 🚀' });
 });
 
-// Démarrage du serveur
+// Starting the server
 app.listen(PORT, () => {
-    console.log(`✅ Serveur lancé sur http://localhost:${PORT}`);
+    console.log(`✅ server started on http://localhost:${PORT}`);
 });
